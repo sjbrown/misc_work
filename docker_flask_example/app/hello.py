d329bcfca9d8977e03ff15380d227566bf2e920a
Shandy Brown <github@ezide.com>
2017-05-31 10:33:06 -0700

    simplified the docker_flask_example.  no uwsgi.ini necessary

--- docker_flask_example/app/hello.py
+++ docker_flask_example/app/hello.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
-app = Flask(__name__)
+hello_app = Flask(__name__)
 
-@app.route("/")
+@hello_app.route("/")
 def hello():
     return "Hello Strange World!"
 
-@app.route("/a/")
+@hello_app.route("/a/")
 def a():
     return "Apples Armadillos!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    hello_app.run(debug=True, host='0.0.0.0')
