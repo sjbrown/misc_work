5f7537ecf336e1fcc34175569f84cb84495a31b6
Shandy Brown <github@ezide.com>
2018-05-19 10:23:02 -0700

    Make HTML during build

--- deckahedron/build_pdf_guides.sh
+++ deckahedron/build_pdf_guides.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+cp ./markdown.css /tmp/markdown.css
+
+ #-s                puts the utf-8 header in
+ #--self-contained  puts data: URLs in
+ #-t html           to HTML
+pandoc \
+ -s \
+ --self-contained \
+ -t html \
+ --css=/tmp/markdown.css \
+ mod_deckahedron_world_gm_guide.md -o /tmp/guide_gm.html
+
 pandoc --reference-odt=custom_pandoc_reference.odt mod_deckahedron_world.md -o /tmp/guide_player.odt
 pandoc --reference-odt=custom_pandoc_reference.odt mod_deckahedron_world_gm_guide.md -o /tmp/guide_gm.odt
 pandoc --reference-odt=custom_pandoc_reference.odt mod_deckahedron_world_campaigns.md -o /tmp/guide_campaigns.odt
@@ -7,3 +19,5 @@ pandoc --reference-odt=custom_pandoc_reference.odt mod_deckahedron_world_campaig
 cd dist
 
 lowriter --headless --convert-to pdf /tmp/guide*.odt
+
+
