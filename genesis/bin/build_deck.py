54895dd6a1c1f9903d7644b39fb0cc0be5fc52d8
Shandy Brown <github@ezide.com>
2021-03-26 22:14:55 -0700

    give the gods some powers

--- genesis/bin/build_deck.py
+++ genesis/bin/build_deck.py
@@ -35,37 +35,46 @@ if __name__ == '__main__':
         print('Provide the name of the deck as the first argument!')
         sys.exit(1)
 
-    outdir = carddir + '/deck_' + sys.argv[1]
-    if os.path.exists(outdir):
-        shutil.rmtree(outdir)
-    os.makedirs(outdir)
-
     card_faces_filenames = [x for x in os.listdir(carddir)
         if (deckname in x)
-        and (x.startswith('card'))
+        and (x.startswith('card_rect'))
         and (x.endswith('.png'))
         and ('back' not in x)
     ]
     card_faces_filenames.sort()
-    card_back_filename = [x for x in os.listdir(carddir)
+    card_backs_filenames = [x for x in os.listdir(carddir)
         if (deckname in x)
-        and (x.startswith('card'))
+        and (x.startswith('card_rect'))
         and (x.endswith('.png'))
         and ('back' in x)
-    ][0]
+    ]
+    card_backs_filenames.sort()
 
-    name_pattern = 'card_tts_%02d.png'
 
-    for (i,fname) in enumerate(card_faces_filenames):
-        i += 1
-        print('copying %s' % fname)
-        shutil.copy(carddir + '/' + fname, outdir + '/' + name_pattern % i)
+    def build(filenames, outdir):
+        if os.path.exists(outdir):
+            shutil.rmtree(outdir)
+        os.makedirs(outdir)
+        name_pattern = 'card_tts_%02d.png'
+        for (i,fname) in enumerate(filenames):
+            i += 1
+            print('copying %s' % fname)
+            shutil.copy(carddir + '/' + fname, outdir + '/' + name_pattern % i)
 
-    deck_svg_fname = outdir + '/deck_' + deckname + '.svg'
-    deck_png_fname = outdir + '/deck_' + deckname + '.png'
-    shutil.copy('./deck_template_tts_7x10.svg', deck_svg_fname)
+        deck_svg_fname = outdir + '/deck_' + deckname + '.svg'
+        deck_png_fname = outdir + '/deck_' + deckname + '.png'
+        shutil.copy('./deck_template_tts_7x10.svg', deck_svg_fname)
+        export_png(deck_svg_fname, deck_png_fname, 3969, 3789)
+        return deck_png_fname
 
-    export_png(deck_svg_fname, deck_png_fname, 3969, 3789)
+    outdir = carddir + '/build_deck_' + sys.argv[1]
+    faces_png_fname = build(card_faces_filenames, outdir)
+    shutil.copy(faces_png_fname, carddir + '/deck_faces_' + sys.argv[1])
 
-    shutil.copy(carddir + '/' + card_back_filename, outdir + '/')
+    if len(card_backs_filenames) == 1:
+        shutil.copy(carddir + '/' + card_backs_filenames[0], outdir + '/')
+    else:
+        outdir = carddir + '/build_deck_back_' + sys.argv[1]
+        backs_png_fname = build(card_backs_filenames, outdir)
+        shutil.copy(backs_png_fname, carddir + '/deck_backs_' + sys.argv[1])
 
