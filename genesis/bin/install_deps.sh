50b65b3ccb5da39ea8fe7b39e46789c794de8163
Shandy Brown <github@ezide.com>
2021-05-29 14:46:41 -0700

    get Netlify to build

--- genesis/bin/install_deps.sh
+++ genesis/bin/install_deps.sh
@@ -7,15 +7,15 @@ if [ $DEBUG ]; then
   set -x
 fi
 
-sudo apt-get update
-sudo apt-get install -y texlive-extra-utils \
+apt-get update
+apt-get install -y texlive-extra-utils \
   inkscape \
   pandoc \
   texlive-full \
   wget
 
 # THIS DOESNT WORK: sudo apt-get install -y python-lxml
-sudo pip install lxml
+pip install lxml
 
 mkdir -p $HOME/.fonts
 wget -O $HOME/.fonts/OptimusPrinceps.ttf https://raw.githubusercontent.com/sjbrown/1kfa/master/publish/OptimusPrinceps.ttf
